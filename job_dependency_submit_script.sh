34804409468e86fbb73b26e492cd47194f3e5354
Richard Massey <rm885@nau.edu>
2020-04-16 22:05:25 -0700

    Update job_dependency_submit_script.sh

--- job_dependency_submit_script.sh
+++ job_dependency_submit_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-array_job_uncert=$(sbatch /scratch/rm885/support/sh/prepare_rf_models_job_array.sh | awk '{ print $4 }')
-sbatch --dependency=afterany:$array_job_uncert/scratch/rm885/support/sh/compile_rf_model_outputs.sh
+array_job=$(sbatch /scratch/user/prepare_job_array.sh | awk '{ print $4 }')
+sbatch --dependency=afterany:$array_job /scratch/user/compile_outputs.sh
